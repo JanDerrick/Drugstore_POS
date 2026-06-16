8efd482d84770d4588a43a3c15a5e62fd5faf464
Jan Derrick Pamplona <pamplonajanderrick@gmail.com>
2026-06-16 17:00:47 +0800

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+//Pamplona, Jan Derrick L.
+//CPE2A-2
+// POS-Drugstore
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        DrugstoreGUI drugstoreGUI = new DrugstoreGUI();
+        drugstoreGUI.setVisible(true);
+        
+    }
+}
